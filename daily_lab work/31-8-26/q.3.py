071eccf01d699647280f45665eb09c426ad60414
Vaidika_ghoghari <ghogharivaidika@gmail.com>
2026-08-31 21:02:20 +0530

    Add files via upload

---
+++ daily_lab work/31-8-26/q.3.py
@@ -0,0 +1,18 @@
+#Q-3
+print("Q-3. takes a boolean values as input and then converts in int,string and print it.")
+
+a=bool(input("enter the first value : "))
+
+int_input=int(a)
+str_input=str(a)
+
+print("Input value: ",a)
+print("input value data type :",type(a))
+
+print("integer value: ",int_input)
+print("data type :",type(int_input))
+
+print("string value: ",str_input)
+print(" data type :",type(str_input))
+
+
---
+++ daily_lab work/31-8-26/q.4.py
@@ -0,0 +1,21 @@
+print("print type using type() and id using id().")
+
+int1=47
+float1=3.14
+str1="hello"
+bool1=True
+list1=["surat","delhi","mubai"]
+tuple1=("jhon","alice","peter")
+dict1={"name":"jhon", "age":23}
+
+print("\n integer value: ",int1, type(int1)," id :",id(int1))
+print("float value: ",float1, type(float1)," id :",id(float1))
+print("string value: ",str1, type(str1)," id :",id(str1))
+print("list value: ",list1, type(list1)," id :",id(list1))
+print("tuple value: ",tuple1, type(tuple1)," id :",id(tuple1))
+print("dictionary value :",dict1, type(dict1)," id :",id(dict1))
+
+
+
+
+
