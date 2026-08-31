3e1fe64760d9a0201f48c27d11a70428f636a92b
vaidika-ghoghari <ghogharivaidika@gmail.com>
2026-08-31 17:19:50 +0530

    Add files via upload

---
+++ daily_lab work/25-8-26/first_script.py
@@ -0,0 +1,9 @@
+name= input("Enter Your Name:")
+
+age= input("Enter your Age:")
+
+school= input("Enter Your School Name:")
+
+print("name :",name)
+print("Age  :",age )
+print("School name :",school)
---
+++ daily_lab work/30-8-26/1.3_lab_work.py
@@ -0,0 +1,41 @@
+#Q-1
+
+print("Q-1. Use of type casting constructor.")
+
+value=input("\nEnetr the number : ")
+f_value= float(value)
+b_value=bool(value)
+int_value=int(value)
+
+
+print("string value : ",  value)
+print("string Type : ", type(value))
+
+print("Int value : ", float(value))
+print("Integer Type : ", type(int_value))
+
+print("float value : ", float(value))
+print("float Type : ", type(f_value))
+
+print("boolean value : ", bool(value))
+print("boolean Type : ", type(b_value))
+
+
+print("-"*70)
+
+#Q-2
+print("Q-2. Convert float value in integer.")
+
+f_input=float(input("Enter your number : "))
+int_input=int(f_input)
+print(f"your enter number is {f_input}. It has a float data type . " )
+print("the data type of your input number ", type(f_input))
+
+print(f"Now conert your input in integer ...", int(f_input) )
+print("the data type of converted input is ", type(int_input))
+
+print("-"*70)
+
+#Q-3
+
+
